8243bc57e1fe0a10ab7fc4d979e7448dec86ce2e
Avinash <vnshhugar@gmail.com>
2026-06-30 12:40:05 +0530

    usestate and useeffect

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -1,12 +1,51 @@
 import Welcome from "./components/welcome";
 import NavBar from "./components/navbar";
+import CompanyCard from "./components/CompanyCard";
+import JobCard from "./components/JobCard";
+import Footer from "./components/Footer";
+import {UseEffect, useState} from "react";
+import {getCompanies} from "./Services/CompanyService";
+import type {Company} from "./types/company"
+
 function App(){
+  const [loading,setLoading] = useState(true);
+  const [error,setError] = useState<Error | null>(null)
+  const [companies,setCompanies] = useState<Company[]>([]);
+
+  async function fetchCompanies(){
+    setLoading(true);
+    try {
+      const companies = await getCompanies();
+      setCompanies(companies);
+    } catch (error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchCompanies();
+  }, []);
+
+if(loading){
+  return <div>Loading...</div>
+}
+
+if (error){
+  return <div>Error:{error.message}</div>
+}
+  
   return (
     <>
       <NavBar />
       <Welcome />
+      <br />
+      <CompanyCard key={companies.id}
+      companies={companies}/>
+      <JobCard />
+      <Footer />
     </>
   )
-}
 
 export default App
--- frontend/talentspark/src/components/CompanyCard.tsx
+++ frontend/talentspark/src/components/CompanyCard.tsx
@@ -1,4 +1,19 @@
+import {getCompanies} from "../Services/CompanyService";
+import {useEffect, useState} from "react";
+import type {Company} from "../types/company";
+
+
 function CompanyCard() {
+
+    const [companies, setCompanies] = useState<Company[]>([]);
+    async function fetchCompanies() {
+        const companies = await getCompanies();
+        setCompanies(companies);
+    }
+    useEffect(() => {
+        fetchCompanies();
+    }, []);
+    
     return (
         <div>
             <h1>Google</h1>
